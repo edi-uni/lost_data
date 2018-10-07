685080a2c25ec5a4acb93384f89a30680a3a901e
Gabriel Badila <gabrielalin9@yahoo.com>
2018-10-07 12:13:02 +0100

    ALL

---
+++ mlpractical-mlp2018-9-lab2/mlp/errors.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""Error functions.
+
+This module defines error functions, with the aim of model training being to
+minimise the error function given a set of inputs and target outputs.
+
+The error functions will typically measure some concept of distance between the
+model outputs and target outputs, averaged over all data points in the data set
+or batch.
+"""
+
+import numpy as np
+
+
+class SumOfSquaredDiffsError(object):
+    """Sum of squared differences (squared Euclidean distance) error."""
+
+    def __call__(self, outputs, targets):
+        """Calculates error function given a batch of outputs and targets.
+
+        Args:
+            outputs: Array of model outputs of shape (batch_size, output_dim).
+            targets: Array of target outputs of shape (batch_size, output_dim).
+
+        Returns:
+            Scalar error function value.
+        """
+        res = outputs - targets
+        E_mean = np.sum(np.sum(res**2, axis=0)) / (2 * outputs.shape[0])
+        return E_mean
+
+    def grad(self, outputs, targets):
+        """Calculates gradient of error function with respect to outputs.
+
+        Args:
+            outputs: Array of model outputs of shape (batch_size, output_dim).
+            targets: Array of target outputs of shape (batch_size, output_dim).
+
+        Returns:
+            Gradient of error function with respect to outputs. This should be
+            an array of shape (batch_size, output_dim).
+        """
+        res = outputs - targets
+        grad = res / outputs.shape[0]
+        return grad
+
+    def __repr__(self):
+        return 'SumOfSquaredDiffsError'
